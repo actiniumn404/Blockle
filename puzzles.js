c920a928165d3444b14d7c82cd2e0d0ace2c5694
actiniumn404 <83673744+actiniumn404@users.noreply.github.com>
2022-09-06 22:39:20 -0700

    License + Info

--- puzzles.js
+++ puzzles.js
@@ -1,42 +1,80 @@
+/*
+MIT License
+
+Copyright (c) 2022 Andrew Chen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+// NOTICE: No, don't skip over this. THis is IMPORTANT.
+// Yes, you found the file with all the answers. There's nothing stopping you from cheating right now, but think about
+// it. Why would you cheat? It doesn't benefit you in any way. By cheating, you are robbing yourself of the fun that
+// comes with finding the solution to the puzzle. Don't cheat okay?
+
 const puzzles = {
-    "2022/9/5": {
+    "2022/9/6": {
         "content": "||1|| ||2|| ||3|| ||4|| testing",
         "hint": "https://bl||ock||le.||alingo||.app",
         "category": "Unknown",
         "solutions": ["https://blockle.alingo.app", "blockle"]
     },
-    "2022/9/6": {
-        "content": "||Run|| ||code|| live in your ||browser||. Write and ||run|| ||code|| in 50+ ||languages|| online with ||Replit||, a powerful ||IDE||, ||compiler||, & ||interpreter||. ",
+    "2022/9/7": {
+        "content": "||Run|| ||code|| live in your ||browser||. Write and ||run|| ||code|| in 50+ ||languages|| " +
+            "online with ||Replit||, a powerful ||IDE||, ||compiler||, & ||interpreter||. ",
         "hint": "https://r||eplit||.com/",
         "category": "Hosting Site",
         "solutions": ["https://replit.com", "replit", "repl.it"]
     },
-    "2022/9/7": {
-        "content": "||Heroku|| is a ||platform|| as a ||service|| (PaaS) that enables ||developers|| to ||build||, run, and operate ||applications|| entirely in the ||cloud||.",
+    "2022/9/8": {
+        "content": "||Heroku|| is a ||platform|| as a ||service|| (PaaS) that enables ||developers|| to ||build||, " +
+            "run, and operate ||applications|| entirely in the ||cloud||.",
         "hint": "https://h||erok||u.com",
         "category": "Hosting Site",
         "solutions": ["https://heroku.com", "heroku"]
     },
-    "2022/9/8": {
-        "content": "||Cool|| ||Math|| ||Games|| is a ||brain||-training site, for everyone, where ||logic||, ||thinking||, ||math|| meets fun ||games||. These ||games|| have no ||violence||, no empty ||action||, just a lot of ||challenges|| that will make you forget you're getting a ||mental|| ||workout||!",
+    "2022/9/9": {
+        "content": "||Cool|| ||Math|| ||Games|| is a ||brain||-training site, for everyone, where ||logic||, " +
+            "||thinking||, ||math|| meets fun ||games||. These ||games|| have no ||violence||, no empty ||action||, " +
+            "just a lot of ||challenges|| that will make you forget you're getting a ||mental|| ||workout||!",
         "hint": "https://co||ol||m||ath||ga||mes||",
         "category": "Games",
         "solutions": ["https://coolmathgames.com", "cool math games", "coolmathgames", "coolmath games"]
     },
-    "2022/9/9": {
+    "2022/9/10": {
         "content": "Online todo ||lists||/||planners|| to make ||school|| ||work|| easier.",
         "hint": "https://to||doli||ster.tk",
         "category": "Unknown (Yeah they seriously do this)",
         "solutions": ["https://todolister.tk", "todolist", "todolister"]
     },
-    "2022/9/10": {
-        "content": "||IMAGINE|| A ||PLACE||...where you can belong to a ||school|| ||club||, a ||gaming|| ||group||, or a ||worldwide|| art ||community||. Where just you and a handful of ||friends|| can spend ||time|| ||together||. A ||place|| that makes it ||easy|| to ||talk|| every day and ||hang|| out more ||often||.",
+    "2022/9/11": {
+        "content": "||IMAGINE|| A ||PLACE||...where you can belong to a ||school|| ||club||, a ||gaming|| ||group||, " +
+            "or a ||worldwide|| art ||community||. Where just you and a handful of ||friends|| can spend ||time|| " +
+            "||together||. A ||place|| that makes it ||easy|| to ||talk|| every day and ||hang|| out more ||often||.",
         "hint": "https://d||iscor||d.gg",
         "category": "Forums.in",
         "solutions": ["discord", "https://discord.gg", "https://discord.com", "https://discordapp.com"]
     },
-    "2022/9/11": {
-        "content": "The ||Oregon|| ||Trail|| was developed as a ||computer|| ||game|| to teach ||school|| ||children|| about the realities of 19th-||century|| ||pioneer|| life on the ||Oregon|| ||Trail||. (Note: Borrowed from Wikipedia)",
+    "2022/9/12": {
+        "content": "The ||Oregon|| ||Trail|| was developed as a ||computer|| ||game|| to teach ||school|| " +
+            "||children|| about the realities of 19th-||century|| ||pioneer|| life on the ||Oregon|| ||Trail||. " +
+            "(Note: Borrowed from Wikipedia)",
         "hint": "https://jamesfriend.com.au/pce-js/mecc/|o||reg||on-t||rai||l.html",
         "category": "games",
         "solutions": ["https://jamesfriend.com.au/pce-js/mecc/oregon-trail.html", "the oregon trail", "oregon trail"]
